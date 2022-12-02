9428fa9dea53648aad2b89d36468ec74d881ee18
Aaron Nixon <aarondoom@cookiedoom.com>
2022-12-02 11:07:53 -0800

    Day 1 complete

---
+++ 1/Elves.php
@@ -0,0 +1,25 @@
+<?php
+$File = fopen("Calories.data", "rb");
+
+$Elf   = 0;
+$Elves = [];
+
+$Calories = 0;
+
+while (!feof($File)) {
+    $Line = trim(fgets($File));
+
+    if (empty($Line)) {
+        $Elves[$Elf] = $Calories;
+
+        ++$Elf;
+        $Calories = 0;
+    }
+    else {
+        $Calories += (int)$Line;
+    }
+}
+
+sort($Elves);
+
+echo array_pop($Elves) . PHP_EOL;
