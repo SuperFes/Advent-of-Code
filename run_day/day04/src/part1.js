5bc6b6f65cf67b0d748dfde302db6701fb1e6e1f
Aaron Nixon <aarondoom@cookiedoom.com>
2024-12-06 18:52:27 -0800

    Day 4 complete

---
+++ run_day/day04/src/part1.js
@@ -0,0 +1,70 @@
+function trampies(lines, X, Y, dirX, dirY) {
+    let str = '';
+
+    let oX = X, oY = Y;
+
+    for (let f = 0; f < 4; f++) {
+        if (lines[oY] !== undefined && lines[oY][oX] !== undefined) {
+            str += lines[oY][oX];
+        }
+        else {
+            // No need to go deeper if they are undefined no matter how far we've gone
+            break;
+        }
+
+        oX += dirX;
+        oY += dirY;
+    }
+
+    if (str.length === 4) {
+        return str === 'XMAS';
+    }
+
+    return false;
+}
+
+function trampStamp(lines, X, Y) {
+    let Matches = 0;
+
+    const dirs = [
+        [1, 0],
+        [-1, 0],
+        [0, 1],
+        [0, -1],
+        [1, 1],
+        [1, -1],
+        [-1, 1],
+        [-1, -1],
+    ];
+
+    dirs.map((dir, i) => {
+        if (trampies(lines, X, Y, dir[0], dir[1])) {
+            Matches++;
+        }
+    });
+
+    return Matches;
+}
+
+function scannerXXX(lines) {
+    let Hits = 0;
+
+    const height = lines.length;
+    const width  = lines[0].length;
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            Hits += trampStamp(lines, x, y);
+        }
+    }
+
+    return Hits;
+}
+
+export default function part1(lines = []) {
+    const Tramps = scannerXXX(lines);
+
+    console.log(`Number of XMAS is: ${Tramps}`);
+
+    return true;
+}
---
+++ run_day/day04/src/part2.js
@@ -0,0 +1,65 @@
+function trampies(lines, X, Y) {
+    let str = '';
+
+    if (lines[Y - 1] !== undefined && lines[Y - 1][X - 1] !== undefined) {
+        str += lines[Y - 1][X - 1];
+
+        if (lines[Y - 1] !== undefined && lines[Y - 1][X + 1] !== undefined) {
+            str += lines[Y - 1][X + 1];
+
+            str += lines[Y][X];
+
+            if (lines[Y + 1] !== undefined && lines[Y + 1][X - 1] !== undefined) {
+                str += lines[Y + 1][X - 1];
+
+                if (lines[Y + 1] !== undefined && lines[Y + 1][X + 1] !== undefined) {
+                    str += lines[Y + 1][X + 1];
+                }
+            }
+        }
+    }
+
+    if (str.length === 5) {
+        return (
+            str === 'MSAMS' ||
+            str === 'MMASS' ||
+            str === 'SMASM' ||
+            str === 'SSAMM'
+        );
+    }
+
+    return false;
+}
+
+function trampStamp(lines, X, Y) {
+    let Matches = 0;
+
+    if (trampies(lines, X, Y)) {
+        Matches++;
+    }
+
+    return Matches;
+}
+
+function scannerXXX(lines) {
+    let Hits = 0;
+
+    const height = lines.length;
+    const width  = lines[0].length;
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            Hits += trampStamp(lines, x, y);
+        }
+    }
+
+    return Hits;
+}
+
+export default function part1(lines = []) {
+    const Tramps = scannerXXX(lines);
+
+    console.log(`Number of X-MAS is: ${Tramps}`);
+
+    return true;
+}
